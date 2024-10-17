9b37228dd674a5f5873a18fb1cbac47f0c528a0c
Orlando Junior <orlandojsjunior@hotmail.com>
2024-10-16 22:14:08 -0300

    Quantos dados temos?
    Consultando tabelas inteiras

---
+++ Aula 1 - Atividade 07 - Quantos dados temos.sql
@@ -0,0 +1,17 @@
+-- Consultando tabelas inteiras
+
+SELECT * FROM categorias;
+
+SELECT * FROM produtos;
+
+
+-- Função COUNT
+
+SELECT COUNT(*) FROM produtos;
+
+SELECT COUNT(*) FROM vendas;
+
+
+-- Criando alias para consultas
+
+SELECT COUNT(*) AS Vendas_Totais FROM vendas;
