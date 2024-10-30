0a5e0bb9c4141c372b27c24dd40a25e0b05644a7
Orlando Junior <orlandojsjunior@hotmail.com>
2024-10-29 22:21:12 -0300

    Update - Dever de casa - Formatando as Query's

--- Aula 3 - Atividade 06 - Dever de casa.sql
+++ Aula 3 - Atividade 06 - Dever de casa.sql
@@ -3,9 +3,9 @@
 
 SELECT strftime("%Y/%m", v.data_venda) AS "Ano/Mes", f.nome AS Nome_Fornecedor, COUNT(iv.produto_id) AS Qtd_Vendas
 FROM itens_venda iv
-JOIN vendas v ON v.id_venda = iv.venda_id
-JOIN produtos p ON p.id_produto = iv.produto_id
-JOIN fornecedores f ON f.id_fornecedor = p.fornecedor_id
+    JOIN vendas v ON v.id_venda = iv.venda_id
+    JOIN produtos p ON p.id_produto = iv.produto_id
+    JOIN fornecedores f ON f.id_fornecedor = p.fornecedor_id
 GROUP BY Nome_Fornecedor, "Ano/Mes"
 ORDER BY "Ano/Mes", Qtd_Vendas
 ;
@@ -27,9 +27,9 @@ trará apenas os dados do fornecedor NebulaNetworks.
 
 SELECT strftime("%Y/%m", v.data_venda) AS "Ano/Mes", f.nome AS Nome_Fornecedor, COUNT(iv.produto_id) AS Qtd_Vendas
 FROM itens_venda iv
-JOIN vendas v ON v.id_venda = iv.venda_id
-JOIN produtos p ON p.id_produto = iv.produto_id
-JOIN fornecedores f ON f.id_fornecedor = p.fornecedor_id
+    JOIN vendas v ON v.id_venda = iv.venda_id
+    JOIN produtos p ON p.id_produto = iv.produto_id
+    JOIN fornecedores f ON f.id_fornecedor = p.fornecedor_id
 WHERE Nome_Fornecedor="NebulaNetworks"
 GROUP BY Nome_Fornecedor, "Ano/Mes"
 ORDER BY "Ano/Mes", Qtd_Vendas
@@ -38,9 +38,9 @@ ORDER BY "Ano/Mes", Qtd_Vendas
 
 SELECT strftime("%Y/%m", v.data_venda) AS "Ano/Mes", COUNT(iv.produto_id) AS Qtd_Vendas
 FROM itens_venda iv
-JOIN vendas v ON v.id_venda = iv.venda_id
-JOIN produtos p ON p.id_produto = iv.produto_id
-JOIN fornecedores f ON f.id_fornecedor = p.fornecedor_id
+    JOIN vendas v ON v.id_venda = iv.venda_id
+    JOIN produtos p ON p.id_produto = iv.produto_id
+    JOIN fornecedores f ON f.id_fornecedor = p.fornecedor_id
 WHERE f.nome="NebulaNetworks"
 GROUP BY f.nome, "Ano/Mes"
 ORDER BY "Ano/Mes", Qtd_Vendas
