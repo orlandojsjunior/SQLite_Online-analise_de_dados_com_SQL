0ca4df23262160cd924a796be42db0f402699e67
Orlando Junior <orlandojsjunior@hotmail.com>
2024-10-30 20:31:06 -0300

    Update - Cálculos de percentual do total
    Formatando e comentando a query

--- Aula 4 - Atividade 07  - Cálculos de percentual do total.sql
+++ Aula 4 - Atividade 07  - Cálculos de percentual do total.sql
@@ -1,15 +1,24 @@
 -- Calculando e exibindo percentuais de vendas
 
 
-SELECT Nome_Categoria, Qtd_Vendas, ROUND(100.0*Qtd_Vendas/(SELECT COUNT(*)
-    FROM itens_venda), 2) || "%" AS Porcentagem
-FROM(
-    SELECT c.nome_categoria AS Nome_Categoria, COUNT(iv.produto_id) AS Qtd_Vendas
-    FROM itens_venda iv
-        JOIN vendas v ON v.id_venda = iv.venda_id
-        JOIN produtos p ON p.id_produto = iv.produto_id
-        JOIN categorias c ON c.id_categoria = p.categoria_id
-    GROUP BY Nome_Categoria
-    ORDER BY Qtd_Vendas DESC
-    )
-;
+SELECT 
+    Nome_Categoria, 
+    Qtd_Vendas, 
+    ROUND(100.0 * Qtd_Vendas / (SELECT COUNT(*) FROM itens_venda), 2) || '%' AS Porcentagem  -- Calcula a porcentagem de vendas
+FROM (
+    SELECT 
+        c.nome_categoria AS Nome_Categoria,  -- Seleciona o nome da categoria
+        COUNT(iv.produto_id) AS Qtd_Vendas  -- Conta o número de produtos vendidos por categoria
+    FROM 
+        itens_venda iv  -- Tabela de itens vendidos
+    JOIN 
+        vendas v ON v.id_venda = iv.venda_id  -- Junta com a tabela de vendas
+    JOIN 
+        produtos p ON p.id_produto = iv.produto_id  -- Junta com a tabela de produtos
+    JOIN 
+        categorias c ON c.id_categoria = p.categoria_id  -- Junta com a tabela de categorias
+    GROUP BY 
+        Nome_Categoria  -- Agrupa os resultados pelo nome da categoria
+    ORDER BY 
+        Qtd_Vendas DESC  -- Ordena os resultados pela quantidade de vendas em ordem decrescente
+);
